fa52e26050d97bc3a5b701506491c3714a0ad490
ActionsCopyBot <micheleemiliani91@gmail.com>
2025-03-05 22:19:05 +0000

    Files updated automatically

--- update-config.sh
+++ update-config.sh
@@ -9,11 +9,12 @@ function print_color() {
 
     case "$color" in
         red)     color_code=0;31 ;;
+        green)   color_code=0;32 ;;
         yellow)  color_code=1;33 ;;
         blue)    color_code=0;34 ;;
-        magenta) color_code=0;35 ;;
+        purple)  color_code=0;35 ;;
         cyan)    color_code=0;36 ;;
-        white)   color_code=0;37 ;;
+        white)   color_code=1;37 ;;
         *)       color_code=0;37 ;;
     esac
     echo -e "\033[${color_code}m${text}\033[0m"
