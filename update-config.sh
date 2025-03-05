364d388b960bafb3c3083989c3076ebfd46b234f
ActionsCopyBot <micheleemiliani91@gmail.com>
2025-03-05 23:00:50 +0000

    Files updated automatically

--- update-config.sh
+++ update-config.sh
@@ -8,14 +8,14 @@ function print_color() {
     local text=$2
 
     case "$color" in
-        red)     color_code=1;31 ;;
-        green)   color_code=0;32 ;;
-        yellow)  color_code=1;33 ;;
-        blue)    color_code=0;34 ;;
-        purple)  color_code=0;35 ;;
-        cyan)    color_code=0;36 ;;
-        white)   color_code=1;37 ;;
-        *)       color_code=0;37 ;;
+        red)     color_code="1;31" ;;
+        green)   color_code="0;32" ;;
+        yellow)  color_code="1;33" ;;
+        blue)    color_code="0;34" ;;
+        purple)  color_code="0;35" ;;
+        cyan)    color_code="0;36" ;;
+        white)   color_code="1;37" ;;
+        *)       color_code="0;37" ;;
     esac
     echo -e "\033[${color_code}m${text}\033[0m"
 }
