25b58a8628a19845e4c8b8a7e09dda15e5c433d4
ActionsCopyBot <micheleemiliani91@gmail.com>
2025-03-05 20:03:09 +0000

    Files updated automatically

--- update-config.sh
+++ update-config.sh
@@ -3,12 +3,12 @@
 # Assume we are running this script after the `update_config()`
 # bash function has pulled the latest changes from the git remote.
 
-set -ex
+set -e
 
 # Update .zshrc_custom and .zshrc_custom_aliases files
 echo "Showing diff between local and remote .zshrc_custom files"
 git --no-pager diff --no-index --color=always ~/.zshrc_custom .zshrc_custom
 
-set +ex
+set +e
 
 echo "Config was updated!!!!!"
